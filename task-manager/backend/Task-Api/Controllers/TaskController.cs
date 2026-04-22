d896a3d8ebf651799df5164d90a06ac81c249ff1
JulianaPelozo <julianapelozopacheco@gmail.com>
2026-04-22 15:47:25 -0300

    commit changes

--- task-manager/backend/Task-Api/Controllers/TaskController.cs
+++ task-manager/backend/Task-Api/Controllers/TaskController.cs
@@ -2,7 +2,6 @@
 
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
-[TaskController]
 public class TaskController : ControllerBase
 {
     private readonly AppDbContext _context;
